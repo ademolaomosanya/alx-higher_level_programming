05fc5d6b6094b0f636c030489f4db850533f1526
Omosanya Ademola <omosanyaademola@gmail.com>
2022-09-12 09:20:46 -0700

    python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(f'{my_list[idx]}', end='')
         except (IndexError):
             continue
-        counter += 1  
+        counter += 1
     print()
     return (counter)
