0367fc7965a349709b2d1bb724d7d7f827a9a50e
Omosanya Ademola <omosanyaademola@gmail.com>
2022-09-12 09:28:19 -0700

    python-exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         print('{:d}'.format(value))
-    except (Exception):        
-        return (False)    
+    except (Exception):
+        return (False)
     return True
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[idx]), end='')
         except (ValueError, TypeError):
-            continue        
+            continue
         counter += 1
     print()
     return (counter)
